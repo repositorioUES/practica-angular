83ccdd707941ebab725d742f47d12f16883c91be
Christian Garcia <kriz10@outlook.com>
2023-08-08 14:20:31 -0600

    first commit

---
+++ src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-reactive',
+  templateUrl: './formulario-reactive.component.html',
+  styleUrls: ['./formulario-reactive.component.css']
+})
+export class FormularioReactiveComponent {
+
+}
---
+++ src/app/formulario-template/formulario-template/formulario-template.component.ts
@@ -0,0 +1,21 @@
+import { compileNgModule } from '@angular/compiler';
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-template',
+  templateUrl: './formulario-template.component.html',
+  styleUrls: ['./formulario-template.component.css']
+})
+export class FormularioTemplateComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  agregar(){
+    console.log()
+  }
+
+}
